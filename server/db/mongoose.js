4d654c4bf0440d9750833552be329ada3f58f953
Karan Keswani <karankeswani99@gmail.com>
2018-12-26 17:17:53 +0530

    Setup App for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://karankeswani99:karan99@ds141813.mlab.com:41813/todoapp');
 module.exports = {mongoose};
