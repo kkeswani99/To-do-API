2e711dbb746ea5d918a45d9e6309584b408190c2
Karan Keswani <karankeswani99@gmail.com>
2018-12-26 15:14:40 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 //Library Imports
 var express    = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 //Local Imports
 var {mongoose} = require('./db/mongoose');
 var {Todo}     = require('./models/todo');
@@ -32,6 +32,32 @@ app.get('/todos', (req,res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+//Getting todos using Id
+app.get('/todos/:id',(req,res) => {
+	var id = req.params.id;
+	//Validating whether id entered by user is valid or not
+	if(!ObjectID.isValid(id))
+	{
+		console.log("ID entered by user is invalid");
+		return res.status(404).send();
+	}
+	else
+	{
+		Todo.findById(id).then((todo) => {
+			if(!todo)
+			{
+				return res.status(404).send();
+			}
+			else
+			{
+				return res.send({todo});
+			}
+		}).catch((e) => {
+			res.status(400).send();
+		});
+	}
 });
 
 app.listen(3000, () => {
