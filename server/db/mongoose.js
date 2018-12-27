492bf357bc9687baa999317b5f91b28608491516
Karan Keswani <karankeswani99@gmail.com>
2018-12-27 20:09:50 +0530

    Set up separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
 //mongoose.connect('mongodb://karankeswani99:karan99@ds141813.mlab.com:41813/todoapp');
+mongoose.connect(process.env.MONGODB_URI);
+
 module.exports = {mongoose};
